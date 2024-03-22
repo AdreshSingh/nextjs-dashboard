5da5c445567399744a4b4a1e2bc201d9536371d7
Arpit Singh <adreshsingh271527@gmail.com>
2024-03-23 00:28:10 +0530

    Initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function page(){
+    return <p className="text-center text-5xl p-10 md:p-12 bg-fuchsia-500 rounded-lg ">Invoices will be here</p>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(){
+    return <p className="h-20 md:h-52 bg-green-400 rounded-lg text-center text-2xl pt-10">Dashboard Page</p>
+}
